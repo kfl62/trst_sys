34860207567f04230a50e263e90690ece5735fbd
kfl62 <kfl62g@gmail.com>
2009-01-22 13:11:42 +0200

    i18n updates

--- app/controllers/trst_users_controller.rb
+++ app/controllers/trst_users_controller.rb
@@ -9,7 +9,7 @@ class TrstUsersController < ApplicationController
   def create
     @trst_user = TrstUser.new(params[:trst_user])
     if @trst_user.save
-      flash[:notice] = "Account registered!"
+      flash[:notice] = t('login.flash.user_create')
       redirect_back_or_default account_url
     else
       render :action => :new
@@ -27,7 +27,7 @@ class TrstUsersController < ApplicationController
   def update
     @trst_user = @current_trst_user # makes our views "cleaner" and more consistent
     if @trst_user.update_attributes(params[:trst_user])
-      flash[:notice] = "Account updated!"
+      flash[:notice] = t('login.flash.user_update')
       redirect_to account_url
     else
       render :action => :edit
