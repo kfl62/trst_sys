76b014069ba29642e566a0be0a9d94dad59cfa26
kfl62 <kfl62g@gmail.com>
2009-01-23 08:44:05 +0200

    js TrstWindow class, Effect.Unfold

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -48,3 +48,105 @@ var Flash = {
     });
   }
 }
+
+var TrstWindow = {
+  public_pg: function(arg1){
+    new Ajax.Request('trst_public/pg?' + arg1,{
+      onCreate: function(request){
+        $('flash-loading').show()
+      },
+      onComplete: function(request){
+        $('flash-loading').hide()
+      }
+    })
+  },
+  info: function(arg1){
+    new Ajax.Request('trst_sys/task?type=info&id=' + arg1,{
+      onCreate: function(request){
+        $('flash-loading').show()
+      },
+      onComplete: function(request){
+        $('flash-loading').hide()
+      }
+    })
+  },
+  exe: function(arg1){
+    new Ajax.Request('trst_sys/task?type=exe&id=' + arg1,{
+      onCreate: function(request){
+        $('flash-loading').show()
+      },
+      onComplete: function(request){
+        $('flash-loading').hide()
+      }
+    })
+  },
+  edit: function(arg1,arg2){
+    new Ajax.Request('trst_sys/task?type=edit&id=' + arg1 + '&obj_id=' + arg2,{
+      onCreate: function(request){
+        $('flash-loading').show()
+      },
+      onComplete: function(request){
+        $('flash-loading').hide()
+      }
+    })
+  },
+  rich_edit: function(arg1,arg2,arg3,arg4){
+    Lightview.show({
+      href: 'trst_sys/rich_edit?type=edit&id=' + arg1 + '&obj_name=' + arg2 + '&obj_id=' + arg3 + '&obj_method=' + arg4,
+      title: "Editor de text",
+      options: {
+        autosize: false,
+        width: 760,
+        height: 490
+      }
+    })
+  }
+};
+
+Effect.Unfold = function(element){
+  element = $(element);
+  var elementDimensions = element.getDimensions();
+  var oldStyle = {
+    top: element.style.top,
+    left: element.style.left,
+    width: element.style.width,
+    height: element.style.height
+  };
+  element.makeClipping();
+  return new Effect.Scale(element, 100, Object.extend({
+    scaleContent: false,
+    scaleY: false,
+    scaleFrom: 1,
+    scaleMode: {
+      originalHeight: elementDimensions.height,
+      originalWidth: elementDimensions.width
+    },
+    afterSetup: function(effect){
+      effect.element.makeClipping().setStyle({
+        width: '1px',
+        height: '10px'
+      }).show();
+    },
+    afterFinishInternal: function(effect){
+      new Effect.Scale(element, 100, {
+        scaleContent: false,
+        scaleX: false,
+        scaleFrom: 5,
+        scaleMode: {
+          originalHeight: elementDimensions.height,
+          originalWidth: elementDimensions.width
+        },
+        restoreAfterFinish: true,
+        afterSetup: function(effect){
+          effect.element.makeClipping().setStyle({
+            height: '1px'
+          }).show();
+        },
+        afterFinishInternal: function(effect){
+          effect.element.undoClipping().setStyle(oldStyle);
+        }
+      });
+    }
+  }, arguments[1] ||
+  {}));
+};
