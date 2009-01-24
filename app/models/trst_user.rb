7a43883b9868d45dd2a5318ac858e36d0649b8ef
kfl62 <kfl62g@gmail.com>
2009-01-24 09:05:41 +0200

    trst_user and account handling [#1 state:resolved]

--- app/models/trst_user.rb
+++ app/models/trst_user.rb
@@ -2,4 +2,14 @@ class TrstUser < ActiveRecord::Base
 
   acts_as_authentic
   
+  def self.per_page
+    20
+  end
+
+  def full_name
+    r = self.frst_name
+    r += '-' + self.mdl_name unless self.mdl_name.nil?
+    r += ' ' + self.lst_name
+  end
+
 end
