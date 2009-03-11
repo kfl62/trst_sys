00d70516be33049d11670c49c64ed40ef7a2492a
kfl62 <kfl62g@gmail.com>
2009-03-11 13:38:17 +0200

    update gem calendar_date_select v.1.15
    some modify to fit for local needs inclusive localization

---
+++ public/javascripts/calendar_date_select/locale/ro.js
@@ -0,0 +1,11 @@
+Date.weekdays = $w('Lu Ma Mi Jo Vi S\u0103 Du');
+Date.months = $w('Ianuarie Februarie Martie Aprilie Mai Iunie Iulie August Septembrie Octombrie Noiembrie Decembrie');
+
+Date.first_day_of_week = 1;
+
+_translations = {
+  "OK": "OK",
+  "Now": "Acum",
+  "Today": "Ast\u0103zi",
+  "Clear": "\u015eterge"
+}
