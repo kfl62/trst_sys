93849da3250218ac7b592b3bcec45af5138f81e3
kfl62 <kfl62g@gmail.com>
2009-03-11 13:35:21 +0200

    using gem calendar_date_select

--- config/environment.rb
+++ config/environment.rb
@@ -26,9 +26,9 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  # config.gem "calendar_date_select"
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "authlogic"
+  config.gem "calendar_date_select", :source => "http://gems.rubyforge.org"
+  config.gem "sqlite3-ruby", :lib => "sqlite3", :source => "http://gems.rubyforge.org"
+  config.gem "authlogic", :source => "http://gems.rubyforge.org"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
