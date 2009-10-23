468e7e49e6fe0bc52cf87a9ee57fbd27c447770d
kfl62 <kfl62g@gmail.com>
2009-10-23 18:44:46 +0300

    rails 2.3.4 and prawn/prawnto for generating pdf

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic", :source => "http://gems.rubyforge.org"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
   config.gem "newrelic_rpm"
+  config.gem "prawn", :source => "http://gems.github.com"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
