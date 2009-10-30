2ea409e1ccf85a6ec0f02c456da07238c04e8912
kfl62 <kfl62g@gmail.com>
2009-10-30 18:27:41 +0200

    no config in session_store :)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -13,4 +13,5 @@
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
 # ActionController::Base.session_store = :active_record_store
-config.action_controller.session_store = :active_record_store
+
+ActionController::Base.session_store = :active_record_store
